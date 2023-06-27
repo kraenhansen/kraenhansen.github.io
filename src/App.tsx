be4daf7eccf29e0860c259782a2d69bbb7fc5451
Kræn Hansen <kraen.hansen@mongodb.com>
2023-06-27 23:54:22 +0200

    Adding links to social media

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
       <h1>
         Hi 👋 I'm Kræn
       </h1>
+      <p>
+        I'm a Software Engineer from Denmark 🇩🇰
+      </p>
+      <p>
+        You can find me on <a href="https://twitter.com/kraenhansen" target="_blank">Twitter</a>,  <a href="https://linkedin.com/in/kraenhansen" target="_blank">LinkedIn</a> and <a href="https://github.com/kraenhansen" target="_blank">GitHub</a>
+      </p>
     </>
   )
 }

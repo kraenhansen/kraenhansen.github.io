d6888b28f58db15f7312b69fd85677296da53100
Kræn Hansen <kraen.hansen@mongodb.com>
2025-02-27 14:52:35 +0100

    Add logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import './App.css'
+import "./App.css";
+import { BlueskyLogo } from "./BlueskyLogo";
+import { GitHubLogo } from "./GitHubLogo";
+import { LinkedInLogo } from "./LinkedInLogo";
 
 function App() {
   return (
     <>
-      <h1>
-        Hi 👋 I'm Kræn
-      </h1>
+      <h1>Hi 👋 I'm Kræn</h1>
+      <p>I'm a Software Engineer from Denmark 🇩🇰</p>
       <p>
-        I'm a Software Engineer from Denmark 🇩🇰
-      </p>
-      <p>
-        You can find me on <a href="https://bsky.app/profile/kraenhansen.dk" target="_blank">🦋</a>, <a href="https://linkedin.com/in/kraenhansen" target="_blank">LinkedIn</a> and <a href="https://github.com/kraenhansen" target="_blank">GitHub</a>
+        You can find me on{" "}
+        <a href="https://bsky.app/profile/kraenhansen.dk" target="_blank">
+          Bluesky <BlueskyLogo />
+        </a>
+        {", "}
+        <a href="https://github.com/kraenhansen" target="_blank">
+          GitHub <GitHubLogo />
+        </a>
+        {" and "}
+        <a href="https://linkedin.com/in/kraenhansen" target="_blank">
+          LinkedIn <LinkedInLogo />
+        </a>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
